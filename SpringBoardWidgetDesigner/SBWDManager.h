4a2b50187d66010c1d1c12dc7ef24f4ff474f17a
cendocathexis-tech <cendocathexis@gmail.com>
2026-09-01 17:59:02 +0300

    Refactor SBWDManager.h to use extern for notifications

--- SpringBoardWidgetDesigner/SBWDManager.h
+++ SpringBoardWidgetDesigner/SBWDManager.h
@@ -1,23 +1,33 @@
+```objc
 #import "SBWDCommon.h"
 
-NSString * const SBWDOpenEditorNotification = @"com.yourname.designer/openEditor";
-NSString * const SBWDReloadNotification = @"com.yourname.designer/reload";
-NSString * const SBWDRespringNotification = @"com.yourname.designer/respring";
+extern NSString * const SBWDOpenEditorNotification;
+extern NSString * const SBWDReloadNotification;
+extern NSString * const SBWDRespringNotification;
 
 @interface SBWDManager : NSObject
+
 + (instancetype)shared;
+
 - (void)ensureDirectories;
 - (void)noteRootFolderView:(UIView *)folderView;
 - (void)openEditorForCurrentPage;
 - (void)handleEditorGesture:(UILongPressGestureRecognizer *)gesture;
 - (void)reloadWidgets;
+
 - (NSDictionary *)hydratedDocumentForPage:(NSUInteger)page;
 - (NSUInteger)currentPageIndex;
 - (UIView *)currentIconListView;
 - (NSDictionary *)documentForPage:(NSUInteger)page;
-- (BOOL)saveDocument:(NSDictionary *)document page:(NSUInteger)page error:(NSError **)error;
+- (BOOL)saveDocument:(NSDictionary *)document
+              page:(NSUInteger)page
+             error:(NSError **)error;
+
 - (NSArray<NSDictionary *> *)availableFonts;
 - (NSString *)fontCSS;
+
 - (NSURL *)editorIndexURL;
 - (NSURL *)runtimeIndexURL;
+
 @end
+```
