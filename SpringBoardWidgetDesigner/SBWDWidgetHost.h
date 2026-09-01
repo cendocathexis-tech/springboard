5269bd48e96bdd778d6b54f82da6139f804ed8d1
cendocathexis-tech <cendocathexis@gmail.com>
2026-09-01 17:48:22 +0300

    Update imports in SBWDWidgetHost.h

--- SpringBoardWidgetDesigner/SBWDWidgetHost.h
+++ SpringBoardWidgetDesigner/SBWDWidgetHost.h
@@ -1,7 +1,10 @@
-#import "SBWDWidgetHost.h"
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface SBWDWidgetHost : NSObject
+
 - (void)attachToListView:(UIView *)listView page:(NSUInteger)page;
 - (void)reload;
 - (void)detach;
+
 @end
